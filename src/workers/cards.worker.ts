a830d0c08b60fd6b5694c96796e5f2f3f12cc5fc
Aviva Ruben <aviva@rubenfamily.com>
2025-11-03 21:32:15 -0600

    fix worker type

--- src/workers/cards.worker.ts
+++ src/workers/cards.worker.ts
@@ -21,11 +21,6 @@ interface CardsWorkerAPI {
 	initialize(): Promise<void>;
 
 	/**
-	 * Check if worker has been initialized
-	 */
-	isInitialized(): boolean;
-
-	/**
 	 * Search cards by name
 	 */
 	searchCards(query: string, limit?: number): Card[];
