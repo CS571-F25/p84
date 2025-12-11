fef67fbade699ab98c6255bab465497486eaa6f5
Aviva Ruben <aviva@rubenfamily.com>
2025-12-10 18:07:03 -0600

    fix url encoding of dids

--- src/router.tsx
+++ src/router.tsx
@@ -13,6 +13,7 @@ export const getRouter = () => {
 		routeTree,
 		context: { ...rqContext },
 		defaultPreload: "intent",
+		pathParamsAllowedCharacters: [":"],
 		Wrap: (props: { children: React.ReactNode }) => {
 			return (
 				<TanstackQuery.Provider {...rqContext}>
